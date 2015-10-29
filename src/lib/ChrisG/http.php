b68b0057f49c2857d1846f255a2d7fae5ff1447b
bamyers99 <bamyers99@10.0.1.2>
2015-10-29 17:21:26 -0400

    Removed deprecated curl_opt

--- src/lib/ChrisG/http.php
+++ src/lib/ChrisG/http.php
@@ -78,7 +78,6 @@ class http {
         curl_setopt($this->ch,CURLOPT_COOKIEFILE,''); // Enable cookie handling
         curl_setopt($this->ch,CURLOPT_COOKIEJAR,'/dev/null'); // Enable cookie handling again because sometimes CURLOPT_COOKIEFILE does not enable.
         curl_setopt($this->ch,CURLOPT_MAXCONNECTS,100);
-        curl_setopt($this->ch,CURLOPT_CLOSEPOLICY,CURLCLOSEPOLICY_LEAST_RECENTLY_USED);
         //curl_setopt($this->ch, CURLOPT_VERBOSE, true);
         $this->postfollowredirs = 0;
         $this->getfollowredirs = 1;
