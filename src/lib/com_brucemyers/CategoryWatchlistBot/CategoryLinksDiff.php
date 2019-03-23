c809f0ea8ac894e6df399de7ac35614c89d26097
bruce <bruce@bruce-Kudu-Professional>
2019-03-22 23:30:59 -0400

    server migration - CategoryWatchlistBot

--- src/lib/com_brucemyers/CategoryWatchlistBot/CategoryLinksDiff.php
+++ src/lib/com_brucemyers/CategoryWatchlistBot/CategoryLinksDiff.php
@@ -156,7 +156,6 @@ class CategoryLinksDiff
     		    $prev_rev_id = $run_rev_id;
     		    $ret = $wiki->getRevisionInfo($prev_rev_id);
     		    $prev_timestamp = $ret['timestamp'];
-    		    echo "prev timestamp1 =" . $prev_timestamp . "\n";
     		}
     	}
 
@@ -166,8 +165,6 @@ class CategoryLinksDiff
         // Want the highest rev_id and the lowest rev_parent_id
 
     	$pages = [];
-    	echo "prev timestamp2 =" . $prev_timestamp . "\n";
-    	echo "cur timestamp =" . MediaWiki::unixTimestampToISO8601Timestamp($cur_timestamp) . "\n";
 
     	$lister = new RecentChangeLister($wiki, $prev_timestamp, MediaWiki::unixTimestampToISO8601Timestamp($cur_timestamp));
 
@@ -192,8 +189,6 @@ class CategoryLinksDiff
     	        }
     	    }
     	}
-
-    	echo "page count =" . count($pages) . "\n";
 
 		krsort($pages); // Reverse so that recent changes has articles first
 
