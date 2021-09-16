7d036a3c35cdb4f8f708880a0a89b0b88b258ab8
bruce <bruce@bruce-Precision-7750>
2021-09-16 16:29:56 -0400

    wikimedia api - new edit token retrieval code

--- src/lib/ChrisG/wikipedia.php
+++ src/lib/ChrisG/wikipedia.php
@@ -340,9 +340,9 @@ class wikipedia {
      * @return edit token.
      **/
     function getedittoken () {
-        $x = $this->query('?action=query&prop=info&intoken=edit&titles=Main%20Page&format=php');
-        foreach ($x['query']['pages'] as $ret) {
-            return $ret['edittoken'];
+        $x = $this->query('?action=query&meta=tokens&type=csrf&format=php');
+        foreach ($x['query']['tokens'] as $ret) {
+            return $ret['csrftoken'];
         }
     }
 
